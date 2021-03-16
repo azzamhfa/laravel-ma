5c91648d3b61495a2c5a891c0ed5a234ad7c8d28
Fathin <fathin210@gmail.com>
2021-03-16 23:51:40 +0000

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,23 +3,22 @@
 @section('title', 'Login')
 
 @section('container')
-    <div style="justify-content: center; align-items: center">
-        <div class="container-sm">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                        placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
+    <link rel="stylesheet" href="css/login.css">
+    <main class="form-signin">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      <label for="inputEmail" class="visually-hidden">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="visually-hidden">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; For Education Purposes Only</p>
+    </form>
+    </main>
 
-    </div>
 @endsection
